8ca6e34d0fa67a9be8e93f2351725feabe0dab8c
Marco Silva <marco.silva@bytewood.com>
2017-03-22 03:13:14 -0300

    Hello World!!!

---
+++ src/main/java/br/net/neuromancer/cmdline/EntryPoint.java
@@ -0,0 +1,18 @@
+package br.net.neuromancer.cmdline;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class EntryPoint {
+	
+	private static Logger logger = LoggerFactory.getLogger(EntryPoint.class);
+
+	public static void main (String[] args){
+		logger.info("Hello World!");
+		
+	}
+	
+}
